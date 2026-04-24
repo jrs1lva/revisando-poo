7b97876e5346ceb1533e4c6c2d558290635ba023
jrs1lva <adailtonjunior0604@gmail.com>
2026-04-24 16:40:26 -0300

    feat: add class models

---
+++ src/com/br/jnasports/model/Bicicleta.java
@@ -0,0 +1,16 @@
+package com.br.jnasports.model;
+
+public class Bicicleta extends Produto {
+
+	public Bicicleta(String nome, double preco) {
+		super(nome, preco);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calcularDesconto() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
